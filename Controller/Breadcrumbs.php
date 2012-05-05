bcb1c1eb7df684be3a4e0ad2a146d41cc53a0115
d1gi <digital_boy@mail.ru>
2012-05-05 20:11:51 +0700

    first commit

---
+++ Controller/Breadcrumbs.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Хлебные крошки (Дублирующая навигация).
+ */
+namespace SmartCore\Module\Breadcrumbs\Controller;
+
+use SmartCore\Bundle\EngineBundle\Engine\Module;
+ 
+class Breadcrumbs extends Module
+{
+	/**
+	 * Разделитель.
+	 * @var string
+	 */
+	protected $delimiter;
+	
+	/**
+	 * Скрыть "хлебные крошки", если выбрана корневая папка.
+	 * @var bool
+	 */
+	protected $hide_if_only_home;
+	
+	/**
+	 * Конструктор.
+	 */
+	protected function init()
+	{
+		$this->NodeProperties->setDefaultParams(array(
+			'delimiter'			=> '&raquo;',
+			'hide_if_only_home'	=> false,
+			));
+
+		$this->View->setOptions(array(
+			'engine'		=> 'simple',
+			'template_ext' => '.tpl',
+		));
+			
+		$this->delimiter		 = $this->NodeProperties->getParam('delimiter');
+		$this->hide_if_only_home = $this->NodeProperties->getParam('hide_if_only_home');
+	}
+	
+	/**
+	 * Запуск модуля.
+	 */
+	public function indexAction($params)
+	{
+		$this->View->delimiter = $this->delimiter;
+		$this->View->Items = $this->Breadcrumbs;
+		$this->View->hide_if_only_home = $this->hide_if_only_home;
+	}	
+}
