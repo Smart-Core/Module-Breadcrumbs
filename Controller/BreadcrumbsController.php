72bf840b1f60853fd20c286e90398e790c2751db
Smart-Core <artem.digi@gmail.com>
2012-07-01 06:01:48 +0700

    рефакторинг API модулей.

--- Controller/BreadcrumbsController.php
+++ Controller/BreadcrumbsController.php
@@ -5,47 +5,42 @@
 namespace SmartCore\Module\Breadcrumbs\Controller;
 
 use SmartCore\Bundle\EngineBundle\Controller\Module;
+use SmartCore\Bundle\EngineBundle\Response;
  
-class Breadcrumbs extends Module
+class BreadcrumbsController extends Module
 {
 	/**
 	 * Разделитель.
 	 * @var string
 	 */
-	protected $delimiter;
+	protected $delimiter = '&raquo;';
 	
 	/**
 	 * Скрыть "хлебные крошки", если выбрана корневая папка.
 	 * @var bool
 	 */
-	protected $hide_if_only_home;
+	protected $hide_if_only_home = false;
 	
 	/**
 	 * Конструктор.
 	 */
 	protected function init()
 	{
-		$this->NodeProperties->setDefaultParams(array(
-			'delimiter'			=> '&raquo;',
-			'hide_if_only_home'	=> false,
-			));
-
 		$this->View->setOptions(array(
 			'engine'		=> 'simple',
 			'template_ext' => '.tpl',
 		));
-			
-		$this->delimiter		 = $this->NodeProperties->getParam('delimiter');
-		$this->hide_if_only_home = $this->NodeProperties->getParam('hide_if_only_home');
 	}
 	
 	/**
 	 * Запуск модуля.
 	 */
-	public function indexAction($params)
+	public function indexAction($router_params = null)
 	{
 		$this->View->delimiter = $this->delimiter;
 		$this->View->Items = $this->Breadcrumbs;
 		$this->View->hide_if_only_home = $this->hide_if_only_home;
+        
+        return new Response($this->View);
 	}	
 }
