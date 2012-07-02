6987b1c61dd5b52a83c73da8eef8ab3437309f09
Smart-Core <artem.digi@gmail.com>
2012-07-03 04:01:46 +0700

    fixed CS

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -6,24 +6,24 @@
  */
 class Module_Breadcrumbs_Admin extends Module_Breadcrumbs implements Admin_ModuleInterface
 {
-	/**
-	 * Получить параметры подключения модуля.
-	 * 
-	 * @return array
-	 */
-	public function getParams()
-	{
-		return array (
-			'delimiter' => array(
-				'label' => 'Разделитель:',
-				'type' => 'text',
-				'value' => $this->delimiter,
-				),
-			'hide_if_only_home' => array(
-				'label' => 'Скрыть, если выбрана корневая папка:',
-				'type' => 'checkbox',
-				'value' => $this->hide_if_only_home,
-				),
-			);
-	}
+    /**
+     * Получить параметры подключения модуля.
+     * 
+     * @return array
+     */
+    public function getParams()
+    {
+        return array (
+            'delimiter' => array(
+                'label' => 'Разделитель:',
+                'type'  => 'text',
+                'value' => $this->delimiter,
+                ),
+            'hide_if_only_home' => array(
+                'label' => 'Скрыть, если выбрана корневая папка:',
+                'type'  => 'checkbox',
+                'value' => $this->hide_if_only_home,
+                ),
+            );
+    }
 }
--- Controller/BreadcrumbsController.php
+++ Controller/BreadcrumbsController.php
@@ -9,38 +9,38 @@ use SmartCore\Bundle\EngineBundle\Response;
  
 class BreadcrumbsController extends Module
 {
-	/**
-	 * Разделитель.
-	 * @var string
-	 */
-	protected $delimiter = '&raquo;';
-	
-	/**
-	 * Скрыть "хлебные крошки", если выбрана корневая папка.
-	 * @var bool
-	 */
-	protected $hide_if_only_home = false;
-	
-	/**
-	 * Конструктор.
-	 */
-	protected function init()
-	{
-		$this->View->setOptions(array(
-			'engine'		=> 'simple',
-			'template_ext' => '.tpl',
-		));
-	}
-	
-	/**
-	 * Запуск модуля.
-	 */
-	public function indexAction($router_params = null)
-	{
-		$this->View->delimiter = $this->delimiter;
-		$this->View->Items = $this->Breadcrumbs;
-		$this->View->hide_if_only_home = $this->hide_if_only_home;
-        
+    /**
+     * Разделитель.
+     * @var string
+     */
+    protected $delimiter = '&raquo;';
+
+    /**
+     * Скрыть "хлебные крошки", если выбрана корневая папка.
+     * @var bool
+     */
+    protected $hide_if_only_home = false;
+
+    /**
+     * Конструктор.
+     */
+    protected function init()
+    {
+        $this->View->setOptions(array(
+            'engine'        => 'simple',
+            'template_ext'  => '.tpl',
+        ));
+    }
+
+    /**
+     * Запуск модуля.
+     */
+    public function indexAction($router_params = null)
+    {
+        $this->View->delimiter = $this->delimiter;
+        $this->View->Items = $this->Breadcrumbs;
+        $this->View->hide_if_only_home = $this->hide_if_only_home;
+
         return new Response($this->View);
-	}	
+    }
 }
