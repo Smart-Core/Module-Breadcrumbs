e6a2d829452c52d65cffaaedc6f1fe00b45455be
Smart-Core <artem.digi@gmail.com>
2016-09-20 01:27:24 +0700

    sf3 migration process

--- Form/Type/NodePropertiesFormType.php
+++ Form/Type/NodePropertiesFormType.php
@@ -4,6 +4,8 @@ namespace SmartCore\Module\Breadcrumbs\Form\Type;
 
 use Smart\CoreBundle\Form\DataTransformer\HtmlTransformer;
 use SmartCore\Bundle\CMSBundle\Module\AbstractNodePropertiesFormType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class NodePropertiesFormType extends AbstractNodePropertiesFormType
@@ -11,11 +13,11 @@ class NodePropertiesFormType extends AbstractNodePropertiesFormType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add($builder->create('delimiter', 'text', [
+            ->add($builder->create('delimiter', TextType::class, [
                 'attr' => ['autofocus' => 'autofocus'],
                 ])->addViewTransformer(new HtmlTransformer(false)))
-            ->add('hide_if_only_home', 'checkbox', ['required' => false])  // Скрыть, если выбрана корневая папка
-            ->add('css_class', 'text', ['required' => false])              // CSS class div блока
+            ->add('hide_if_only_home', CheckboxType::class, ['required' => false])  // Скрыть, если выбрана корневая папка
+            ->add('css_class', TextType::class, ['required' => false])              // CSS class div блока
         ;
     }
 
